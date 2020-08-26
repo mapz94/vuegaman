f917671bf79801b464c0d8ed35d519533b02a02f
Miguel Perez <miguel.apg94@gmail.com>
2020-08-26 15:36:23 -0400

    Logo Transition

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,79 @@
 <template>
-  <v-app>
-    <img class="filter-green" src="@/assets/logo_gaman_horse.svg"/>
+  <v-app style="background-color: rgb(32,32,64)">
+    <div style="color: white; font-size: 3vw; text-align: center;">GAMAN</div>
+    <img class="logo-filter" src="@/assets/logo_gaman_horse.svg"/>
   </v-app>
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
+  data () {
+    return{
+      hue: 180,
+    }
+  },
   components: {
 
   },
+  methods:{
 
-  data: () => ({
-    //
-  }),
+  },
+  computed:{
+
+  }
+
 };
 </script>
 
 <style>
-.filter-green{
-      filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
+.logo-filter{
+  animation: hue-rotation 4s ease 0s infinite;
+}
+  @keyframes hue-rotation {
+    0%,
+    5%{
+      filter: hue-rotate(18deg)
+    }    10%{
+      filter: hue-rotate(36deg)
+    }    15%{
+      filter: hue-rotate(54deg)
+    }    20%{
+      filter: hue-rotate(72deg)
+    }    25%{
+      filter: hue-rotate(90deg)
+    }    30%{
+      filter: hue-rotate(108deg)
+    }    35%{
+      filter: hue-rotate(122deg)
+    }    40%{
+      filter: hue-rotate(140deg)
+    }    45%{
+      filter: hue-rotate(158deg)
+    }    50%{
+      filter: hue-rotate(172deg)
+    }    55%{
+      filter: hue-rotate(190deg)
+    }    60%{
+      filter: hue-rotate(208deg)
+    }    65%{
+      filter: hue-rotate(226deg)
+    }    70%{
+      filter: hue-rotate(244deg)
+    }    75%{
+      filter: hue-rotate(272deg)
+    }    80%{
+      filter: hue-rotate(290deg)
+    }    85%{
+      filter: hue-rotate(308deg)
+    }    90%{
+      filter: hue-rotate(326deg)
+    }    95%{
+      filter: hue-rotate(344deg)
+    }    100%{
+      filter: hue-rotate(360deg)
     }
+    
+  }
 
 </style>
