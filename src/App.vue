651525215e116c5372a7bc9e960d2a073f4a1460
miguel-apg <51242040+miguel-apg@users.noreply.github.com>
2020-10-15 13:43:44 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <v-app id="gaman">
+  <v-app>
     <navigationDrawer/>
-    <div :style=invert>
-      <img class="item-vector" src="@/assets/bottom-curved-rect.svg">
-    </div>
-    
     <appBar/>
-      
-    <v-main>
+    <v-main >
       <v-container fluid>
-        <home/>
-        
-        <sandBox/>
+        <transition name="fade" mode="out-in"> <!-- Curve -->
+          <router-view/>
+        </transition>
       </v-container>
     </v-main>
-
   <theFooter/>
   </v-app>
 </template>
-
 <script>
   export default {
     data(){
@@ -29,8 +22,6 @@
     components:{
       navigationDrawer: () => import ("@/components/TheNavigationDrawer"),
       appBar: () => import ("@/components/TheAppBar"),
-      home: () => import ("@/views/Home"),
-      sandBox: () => import ("@/components/SandBox"),
       theFooter: () => import("@/components/TheFooter")
     },
     mounted(){
@@ -45,7 +36,17 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@600&display=swap');
-#gaman{
+.fade-enter-active,
+.face-leave-active{
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to{
+  opacity: 0;
+}
+
+
+.gradient-fill{
   background: rgb(255,0,200);
   background: linear-gradient(270deg, rgba(255,0,200,0.75) 0%, rgba(255,50,128,1) 100%);
 }
